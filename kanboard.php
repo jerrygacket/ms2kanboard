c9017130a0dc969fc58b3f3ba9ef943886ce6e6d
jerrygacket <jerrygacket@gmail.com>
2017-04-25 16:57:19 +0400

    comment edited and code arranged

--- kanboard.php
+++ kanboard.php
@@ -17,43 +17,49 @@ $kanboardclient = new Client([
 
 //***************************************************
 //see kanboard api documentation for more detail about users and access keys
+//you must replace $kanboardAccessKey with your key
 $kanboardAccessKey = '4620d453e52d23a33cead8996fdfe2f7d59a7b73ba77456874098509d4d3';
 $kanboardName="jsonrpc";
 //***************************************************
 
+//project id and column id must be replaced with your data
 $project_id = 45;
 $zero_column_id = 789;
 
 switch ($modx->event->name) {
-    case "msOnCreateOrder":
-		$order_id = "{$msOrder->num}"; 														//get order id
-		$contacts = $modx->getObject('msOrderAddress', array('id'=> $msOrder->address));	//get customer contacts
-        $_products = $msOrder->getMany('Products');											//get products in shoping cart
-        
-        $i = 0;
-        $products = '';
-        foreach($_products as $product) {													//make strings for every product for task description
-            $i++;
-            $products .= "{$i}. {$product->name} ({$product->count} шт.) {$product->price} руб./шт. Всего {$product->cost} рублей";
-            $products .= "\n";
-        }
-        
-        //make description for task with markdown language
+	case "msOnCreateOrder":
+		//get order id
+		$order_id = "{$msOrder->num}";
+		//get customer contacts
+		$contacts = $modx->getObject('msOrderAddress', array('id'=> $msOrder->address));
+		//get products in shoping cart
+        	$_products = $msOrder->getMany('Products');
+
+		$i = 0;
+		$products = '';
+		foreach($_products as $product) {
+			$i++;
+			//make strings for every product for task description
+			$products .= "{$i}. {$product->name} ({$product->count} шт.) {$product->price} руб./шт. Всего {$product->cost} рублей";
+			$products .= "\n";
+		}
+
+		//make description for task with markdown language
 		$description .= "Новый заказ #{$msOrder->num} на сумму {$msOrder->cost} р.\n\n";
-        $description .= "Товары:\n";
-        $description .= "{$products}\n";
-        $description .= "Доставка по адресу:\n";
-        $description .= "* Получатель: {$contacts->receiver}\n";
-        $description .= "* Индекс: {$contacts->index}\n";
-        $description .= "* Область: {$contacts->region}\n";
-        $description .= "* Город: {$contacts->city}\n";
-        $description .= "* Улица: {$contacts->street}\n";
-        $description .= "* Дом: {$contacts->building}\n";
-        $description .= "* Квартира: {$contacts->room}\n";
-        $description .= "* Комментарий: {$contacts->comment}\n";
-        $description .= "\n";
-		
-		//create task in kanboard
+		$description .= "Товары:\n";
+		$description .= "{$products}\n";
+		$description .= "Доставка по адресу:\n";
+		$description .= "* Получатель: {$contacts->receiver}\n";
+		$description .= "* Индекс: {$contacts->index}\n";
+		$description .= "* Область: {$contacts->region}\n";
+		$description .= "* Город: {$contacts->city}\n";
+		$description .= "* Улица: {$contacts->street}\n";
+		$description .= "* Дом: {$contacts->building}\n";
+		$description .= "* Квартира: {$contacts->room}\n";
+		$description .= "* Комментарий: {$contacts->comment}\n";
+		$description .= "\n";
+
+			//create task in kanboard
 		$kanboardresponse = $kanboardclient->request('POST', 'jsonrpc.php', [
 			'auth' => [$kanboardName, $kanboardAccessKey],
 			'json' => [
@@ -68,76 +74,80 @@ switch ($modx->event->name) {
 				]
 			]
 		]);
+		break;
+	case "msOnChangeOrderStatus":
+		//get order id
+		$order_id = $order->get('num');
+		//set column id in kanboard project in accordance with order status
+		//must be done with case select or for search column id in project with keywords
+		//or modx_lexicon if it will be a modx component
+		$column_id = $zero_column_id + $status;
+		//get payment info
+		$_paymentinfo = json_decode("{$order->properties}",true);
+		//if payment info available then make strings for task deskription 
+		if (!empty($_paymentinfo['payment'])){
+		    $paymentinfo = "Информация о платеже:\n";
+		    foreach($_paymentinfo['payment'] as $key => $value) {
+			$paymentinfo .= "* $key: $value";
+			$paymentinfo .= "\n";
+		    }
+		}
+
+		//search task in project. query is order id.
+		$kanboardresponse = $kanboardclient->request('POST', 'jsonrpc.php', [
+			'auth' => [$kanboardName, $kanboardAccessKey],
+			'json' => [
+				'jsonrpc' => '2.0',
+				'method'=>'searchTasks',
+				'id'=>'1',
+				'params'=>[
+					'project_id'=>$project_id,
+					'query'=>$order_id
+				]
+			]
+		]);
+		$kanboardbody = $kanboardresponse->getBody();
+		$kanboardjsonResponse=json_decode($kanboardbody,true);
 		
-        break;
-    case "msOnChangeOrderStatus":
-        $order_id = $order->get('num');												//get order id
-        $column_id = $zero_column_id + $status;										//set column id in kanboard project in accordance with order status
-																					//must be done with case select or for search column id in project with keywords
-																					//or modx_lexicon if it will be a modx component
-        $_paymentinfo = json_decode("{$order->properties}",true);					//get payment info
-        if (!empty($_paymentinfo['payment'])){										//if payment info available then make strings for task deskription 
-            $paymentinfo = "Информация о платеже:\n";
-            foreach($_paymentinfo['payment'] as $key => $value) {
-                $paymentinfo .= "* $key: $value";
-                $paymentinfo .= "\n";
-            }
-        }
-        
-        //search task in project. query is order id.
-    	$kanboardresponse = $kanboardclient->request('POST', 'jsonrpc.php', [
-    		'auth' => [$kanboardName, $kanboardAccessKey],
-    		'json' => [
-    			'jsonrpc' => '2.0',
-    			'method'=>'searchTasks',
-    			'id'=>'1',
-    			'params'=>[
-    				'project_id'=>$project_id,
-    				'query'=>$order_id
-    			]
-    		]
-    	]);
-    	$kanboardbody = $kanboardresponse->getBody();
-    	$kanboardjsonResponse=json_decode($kanboardbody,true);
-        
-        if(empty($kanboardjsonResponse['error'])) {									
-            if(!empty($kanboardjsonResponse['result'])) {							//if task with title $order_id exists
-                $task_id = $kanboardjsonResponse['result'][0]['id'];
-                $description = $kanboardjsonResponse['result'][0]['description'];
-                $description .= "\n";
-                $description .= $paymentinfo;
-                
-                //update task with payment info in description
-        		$kanboardresponse = $kanboardclient->request('POST', 'jsonrpc.php', [
-        			'auth' => [$kanboardName, $kanboardAccessKey],
-        			'json' => [
-        				'jsonrpc' => '2.0',
-        				'method'=>'updateTask',
-        				'id'=>'1',
-        				'params'=>[
-        					'id' => $task_id,
-        					'description' => $description,
-        				]
-        			]
-        		]);
-        		
-        		//move task into different column
-        		$kanboardresponse = $kanboardclient->request('POST', 'jsonrpc.php', [
-        			'auth' => [$kanboardName, $kanboardAccessKey],
-        			'json' => [
-        				'jsonrpc' => '2.0',
-        				'method'=>'moveTaskPosition',
-        				'id'=>'1',
-        				'params'=>[
-        					'project_id' => $project_id,
-                            'task_id' => $task_id,
-                            'column_id' => $column_id,
-                            'position' => 1,
-                            'swimlane_id' => 12,
-        				]
-        			]
-        		]);
-            }
-        }
-        break;
+		//if no error on json request and task with title $order_id exists
+		if(empty($kanboardjsonResponse['error'])) {
+		    if(!empty($kanboardjsonResponse['result'])) {
+			$task_id = $kanboardjsonResponse['result'][0]['id'];
+			$description = $kanboardjsonResponse['result'][0]['description'];
+			$description .= "\n";
+			$description .= $paymentinfo;
+
+			//update task with payment info in description
+			$kanboardresponse = $kanboardclient->request('POST', 'jsonrpc.php', [
+				'auth' => [$kanboardName, $kanboardAccessKey],
+				'json' => [
+					'jsonrpc' => '2.0',
+					'method'=>'updateTask',
+					'id'=>'1',
+					'params'=>[
+						'id' => $task_id,
+						'description' => $description,
+					]
+				]
+			]);
+
+			//move task into different column
+			$kanboardresponse = $kanboardclient->request('POST', 'jsonrpc.php', [
+				'auth' => [$kanboardName, $kanboardAccessKey],
+				'json' => [
+					'jsonrpc' => '2.0',
+					'method'=>'moveTaskPosition',
+					'id'=>'1',
+					'params'=>[
+						'project_id' => $project_id,
+			    'task_id' => $task_id,
+			    'column_id' => $column_id,
+			    'position' => 1,
+			    'swimlane_id' => 12,
+					]
+				]
+			]);
+		    }
+		}
+		break;
 }
